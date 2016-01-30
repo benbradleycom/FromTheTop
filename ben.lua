14509a55a87b4d01aec7f2f08b32f0ca3e6db03a
Ben Bradley <auto@benbradley.com>
2016-01-30 15:14:05 +0000

    scale and translation system
    need to deep copy tables for AddOne

--- ben.lua
+++ ben.lua
@@ -16,41 +16,49 @@ local win = am.window{
 panels = { count = 0, nodeTable = {} }
 
 function PanelScaleControl(node)
-	node.scale = node.pscale
+	if node.p.state == PanelState.docked then
+		node.p.scale = 0.2
+	end
+	
+	node.scale = vec3(node.p.scale)
 end
 
 function PanelPosControl(node)
-	node.translate = vec3(0,100,0)
+	dockedPos = vec3(-300 + (node.p.index * 120), -200, 0)
+	if node.p.state == PanelState.docked then
+		node.p.pos = dockedPos
+	end
+	
+	node.position = node.p.pos
 end
 
 PanelState =
 {
 	docked = 1,
-	animOn = 2,
+	animOn = 2, 
 	teaching = 3,
 	active = 4,
 	animOff = 5,
 }
 
-PanelActivity =
+PanelActivity = 
 {
 	-- array of default table contents
 	{ -- word
 		node = am.scale(1):action(PanelScaleControl)
-			^ am.translate(0, -25):action(PanelPosControl)
+			^ am.translate(0,0):action(PanelPosControl)
 			^ am.rect(-50, -50, 50, 50, yellow),
 		text = "dog",
-		pscale = vec3(15,5,1),
 	},
 	{ -- pattern
-		node = am.scale(1)
-			^ am.translate(-40, -50)
+		node = am.scale(1):action(PanelScaleControl)
+			^ am.translate(0,0):action(PanelPosControl)
 			^ am.rect(-50, -50, 50, 50, orange),
 		dots = 4,
 	},
 	{ -- drums
-		node = am.scale(1)
-			^ am.translate(40, -30)
+		node = am.scale(1):action(PanelScaleControl)
+			^ am.translate(0,0):action(PanelPosControl)
 			^ am.rect(-50, -50, 50, 50, pink),
 		snare = true,
 	},
@@ -62,11 +70,6 @@ PanelActivity =
 	--
 	count = 3,
 }
-
-wordpanel = PanelActivity[1]
-function wordpanel.node:get_pscale()
-	return wordpanel.pscale
-end
 
 
 function panels.MakeEmpty()
@@ -83,20 +86,26 @@ function panels.AddOne(activity)
 		activity = math.random(1,PanelActivity.count)
 	end
 	
+	log(activity)
+	
 	local p = 
 	{	-- here we define the panel table parameters
 		-- these are used to communicate from the panel to the game level
 		-- we'll give them all default values rather than nil
-		scale = vec3(3,5,1),
-		position = vec2( win.width, win.height ) * 0.5,
+		scale = 1,
+		pos = vec2( win.width, win.height ) * 0.5,
 		state = PanelState.docked,
 		activity = activity
 	}
 	
 	table.merge( p, PanelActivity[activity] )
+	function p.node:get_p() return p end -- provide node a getter to parent
+	translate = p.node"translate"
+	function translate:get_p() return p end
 	
 	-- last of all, add p into the table and the node to the scene table
 	panels.count = panels.count + 1
+	p.index = panels.count
 	panels[panels.count] = p
 	panels.nodeTable[panels.count] = p.node
 end
@@ -107,7 +116,7 @@ end
 -------------------------------------------------------
 
 -- test panels
-for n=1, 6 do
+for n=1, 7 do
 	panels.AddOne()
 end
 
