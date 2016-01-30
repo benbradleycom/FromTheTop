dc8a5e5e677fd72bc2972e6730fb6d1416d55824
Ben Bradley <auto@benbradley.com>
2016-01-30 15:51:39 +0000

    row of panels with one active

--- ben.lua
+++ ben.lua
@@ -4,9 +4,9 @@ local orange = vec4(1, 0.5, 0, 1)
 local pink = vec4(1,0,0.5,1)
 
 local win = am.window{
-    title = "From The Top ~ ben's testbed",
+    title = "From The Top",
     width = 1200,
-    height = 800,
+    height = 750,
     clear_color = vec4(0.3),
 }
 
@@ -16,17 +16,23 @@ local win = am.window{
 panels = { count = 0, nodeTable = {} }
 
 function PanelScaleControl(node)
+
 	if node.p.state == PanelState.docked then
 		node.p.scale = 0.2
-	end
+	elseif node.p.state == PanelState.input then
+		node.p.scale = 4
+	end	
 	
 	node.scale = vec3(node.p.scale)
 end
 
 function PanelPosControl(node)
-	dockedPos = vec3(-300 + (node.p.index * 120), -200, 0)
+	dockedPos = vec3(-550 + (node.p.index * 40), 320, 0)
+	
 	if node.p.state == PanelState.docked then
 		node.p.pos = dockedPos
+	elseif node.p.state == PanelState.input then
+		node.p.pos = vec3(0)
 	end
 	
 	node.position = node.p.pos
@@ -37,7 +43,7 @@ PanelState =
 	docked = 1,
 	animOn = 2, 
 	teaching = 3,
-	active = 4,
+	input = 4,
 	animOff = 5,
 }
 
@@ -65,39 +71,34 @@ function panels.AddOne(activity)
 		activity = math.random(1,PanelActivity.count)
 	end
 	
-	log(activity)
-	 
-	local p = 
+	local p =
 	{	-- here we define the panel table parameters
 		-- these are used to communicate from the panel to the game level
 		-- we'll give them all default values rather than nil
 		scale = 1,
-		pos = vec2( win.width, win.height ) * 0.5,
+		pos = vec3( win.width, win.height, 0 ) * 0.5,
 		state = PanelState.docked,
-		activity = activity
+		activity = activity,
+		node = am.translate(0,0):action(PanelPosControl)
+			^ am.scale(1):action(PanelScaleControl),
 	}
 	
+	scaleNode = p.node"scale"
+	
 	if activity == PanelActivity.word then
-		p.node = am.scale(1):action(PanelScaleControl)
-			^ am.translate(0,0):action(PanelPosControl)
-			^ am.rect(-50, -50, 50, 50, yellow)
+		scaleNode = scaleNode ^ am.rect(-50, -50, 50, 50, yellow)
 		p.text = "dog"
 	elseif activity == PanelActivity.pattern then
-		p.node = am.scale(1):action(PanelScaleControl)
-			^ am.translate(0,0):action(PanelPosControl)
-			^ am.rect(-50, -50, 50, 50, orange)
+		scaleNode = scaleNode ^ am.rect(-50, -50, 50, 50, orange)
 		p.dots = 4
 	elseif activity == PanelActivity.drums then
-		p.node = am.scale(1):action(PanelScaleControl)
-			^ am.translate(0,0):action(PanelPosControl)
-			^ am.rect(-50, -50, 50, 50, pink)
+		scaleNode = scaleNode ^ am.rect(-50, -50, 50, 50, pink)
 		p.snare = true
 	end
 	
-	
-	function p.node:get_p() return p end -- provide node a getter to parent
-	translate = p.node"translate"
-	function translate:get_p() return p end
+	 -- provide nodes a getter to parent
+	function p.node:get_p() return p end
+	function scaleNode:get_p() return p end
 	
 	-- last of all, add p into the table and the node to the scene table
 	panels.count = panels.count + 1
@@ -115,6 +116,8 @@ end
 for n=1, 7 do
 	panels.AddOne()
 end
+
+panels[3].state = PanelState.input
 
 win.scene = am.group() ^ panels.nodeTable
 
